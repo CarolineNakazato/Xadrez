c9fa334365077b0c9f33024fc29935c2c0fe3174
CarolineNakazato <carolinenakazato@yahoo.com.br>
2019-10-06 15:47:00 -0300

    Add files via upload

--- tabuleiro.js
+++ tabuleiro.js
@@ -1,18 +1,25 @@
-function tabuleiro(peca, i, j) {
-	this.getPeca = function() {
-		return peca;
-	}
-	this.getI = function() {
-		return i;
-	}
-
-	this.getJ = function() {
-		return horaSaida;
-	}
-
-	this.setPeca = function(_peca) {
-		peca = _peca;
+function tabuleiro() {
+	var tabuleiro = new Array(8);
+	
+	this.iniciar = function() {
+		tabuleiro[i] = new Array(8);
+			for(j = 0; j < 8; j++)
+				tabuleiro[i][j] = 0; 
+		}
+		return tabuleiro;
 	}
 	
+	this.getPeca = function(i,j) {
+		return tabuleiro[i][j];
+	}
+	this.getRepresentacao = function() {
+		return tabuleiro;
+	}
 	
+	this.addPeca = function(peca){// esse m ́etodo deve colocar a pe ̧ca na posi ̧c ̃ao i, j no tabuleiro.
+		tabuleiro[peca.getI()][peca.getJ()] = peca;
+	}
+	this.rmPeca= function(i,j){// esse m ́etodo deve remover a pe ̧ca da posi ̧c ̃ao i, j do tabuleiro.
+		return tabuleiro[i][j] = 0;
+	}
 }
